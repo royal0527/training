2163eed6aedb4f3db4dbcc714768eebfabeac95e
rajusk0527 <rajas2096@gmail.com>
2022-03-23 16:45:16 +0530

    k

---
+++ training/src/training/Mycomparator.java
@@ -0,0 +1,13 @@
+package training;
+
+import java.util.Comparator;
+
+public class Mycomparator implements Comparator {
+
+	@Override
+	public int compare(Object o1, Object o2) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
